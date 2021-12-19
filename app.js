bf980655a842d42c990cbfd10123d2bfa4a4ac8d
fawriid <farid.miri.1393@gmail.com>
2021-12-19 22:30:05 +0330

    hiding mobile menu feature added (when you open hamburger manu and click on an item menu will be closed automatically)

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ const mobileNavbar = () => {
   });
 };
 mobileNav.addEventListener("click", mobileNavbar);
+
+const hideMobileNav = () => {
+  const mobileBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 1024 && mobileBars) {
+    bars.forEach((e) => {
+      e.classList.toggle("is-active");
+    });
+    navLinks.classList.remove("active");
+  }
+};
+const logo = document.querySelector(".nav__logo");
+logo.addEventListener("click", hideMobileNav);
+navLinks.addEventListener("click", hideMobileNav);

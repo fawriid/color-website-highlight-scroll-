b41916b72b3b365c7d53fc9293a20fce6d927599
fawriid <farid.miri.1393@gmail.com>
2021-12-20 00:43:08 +0330

    highlight under focus section added for navbar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// NAVBAR FOR MOBILE
 const navLinks = document.querySelector(".nav__wrapper .nav__links");
 const mobileNav = document.querySelector("#mobile__nav");
 const bars = document.querySelectorAll(".bars");
@@ -10,6 +11,7 @@ const mobileNavbar = () => {
 };
 mobileNav.addEventListener("click", mobileNavbar);
 
+// HIDING NAVBAR
 const hideMobileNav = () => {
   const mobileBars = document.querySelector(".is-active");
   if (window.innerWidth <= 1024 && mobileBars) {
@@ -22,3 +24,35 @@ const hideMobileNav = () => {
 const logo = document.querySelector(".nav__logo");
 logo.addEventListener("click", hideMobileNav);
 navLinks.addEventListener("click", hideMobileNav);
+
+// HIGHLIGHT FOR NAVBAR ITEM
+const scroolHighlight = () => {
+  const elem = document.querySelector(".highlight");
+  const home = document.querySelector("#home-page");
+  const about = document.querySelector("#about-page");
+  const services = document.querySelector("#services-page");
+  const signup = document.querySelector("#signup-page");
+  const webWidth = window.innerWidth;
+  let scrollPos = window.scrollY;
+  if (webWidth > 1024 && scrollPos < 725) {
+    home.classList.add("highlight");
+    about.classList.remove("highlight");
+    return;
+  } else if (webWidth > 1024 && scrollPos < 1525) {
+    home.classList.remove("highlight");
+    about.classList.add("highlight");
+    services.classList.remove("highlight");
+    return;
+  } else if (webWidth > 1024 && scrollPos < 2300) {
+    services.classList.add("highlight");
+    about.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("click", scroolHighlight);
+window.addEventListener("scroll", scroolHighlight);
